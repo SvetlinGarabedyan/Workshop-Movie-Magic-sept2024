00b3002b00889f78ad5637ffbf446b992e96adb9
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-10-15 00:58:48 +0200

    Setup Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Just Works');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Just Works!!!');
+})
+
+
+app.listen(1000, () => console.log('Server is listening on port http://localhost:1000...'))
